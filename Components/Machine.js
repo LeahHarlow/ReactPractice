32787a2947db1dc317608d72a9dd2c127e0ca594
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-26 13:57:52 -0700

    moving on

--- Components/Machine.js
+++ Components/Machine.js
@@ -1,7 +1,7 @@
 class Machine extends React.Component {
   render() {
-    const { s1, s2, s3 } = this.props;
-    const winner = s1 === s2 && s2 === s3;
+    const { s1, s2, s3 } = this.props; //destructuring off props
+    const winner = (s1 === s2) && (s2 === s3);
 
     return (
       <div>
